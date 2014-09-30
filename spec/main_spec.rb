d303787be20d9060acbf61e8a11db53f5e0ddbd1
Adrian Pradilla <adrianpradilla@ideas4all.com>
2014-09-30 15:41:37 +0200

    Initial commit

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe "Application 'RubyMotionGoogleAnalyticsSDKiOSv3'" do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "has one window" do
+    @app.windows.size.should == 1
+  end
+end
